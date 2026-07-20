ed310165780f8a85ee593e54589d5b7ecb96bbe5
abhinav tripathi <abhinavtripathi302@gmail.com>
2026-07-21 01:01:49 +0530

    Day 1: Added sum of number

---
+++ Day1/hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n1, n2;
+    cout << "Enter two numbers: ";
+    cin >> n1 >> n2;
+    cout << "Sum: " << n1 + n2 << endl;
+    return 0;
+}
