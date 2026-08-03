b9f0edc3a30cbeafa26d1853bcd34b18a1df964b
abhinav tripathi <abhinavtripathi302@gmail.com>
2026-08-03 20:39:17 +0530

    Day12 trasfering zero at the end of the array

---
+++ Day12/moving_zeros_at_end.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int n;
+    cout << "Enter size: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter array elements: ";
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+
+    for(int i = 0; i < n; i++){
+
+        if(arr[i] == 0){
+
+            int temp = arr[i];
+
+            for(int j = i; j < n - 1; j++){
+                arr[j] = arr[j + 1];
+            }
+
+            arr[n - 1] = temp;
+
+            i--;      
+            n--;      
+        }
+    }
+
+    cout << "Array after moving zeros to end: ";
+
+    for(int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
