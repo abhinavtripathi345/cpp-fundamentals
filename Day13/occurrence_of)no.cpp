d63beccf75aa9d440162fc34072218b450b35b50
abhinav tripathi <abhinavtripathi302@gmail.com>
2026-08-08 20:14:05 +0530

    day13 occurance of no. in a array

---
+++ Day13/occurrence_of)no.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter the value of n";
+    cin>>n;
+    int arr[n];
+    cout<<"enter the element of the array";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    for(int i=0;i<n;i++){
+        int count=0;
+        for(int j=0;j<n;j++){
+            if(arr[i]==arr[j]){
+                count++;
+                
+            }
+        }
+        cout<<"element "<<arr[i]<<" occurrence= "<<count<<endl;
+    }
+    return 0;
+}
