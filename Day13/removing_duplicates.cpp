aefc2d9613d62f0f69b23fa07bb380812dca123b
abhinav tripathi <abhinavtripathi302@gmail.com>
2026-08-08 20:29:46 +0530

    Day13 removing duplicates in an array

---
+++ Day13/removing_duplicates.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter thr value of n";
+    cin>>n;
+    int arr[n];
+    cout<<"enter the element of the array";
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+
+    }
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            if(arr[i]==arr[j]){
+                for(int k=j;k<n-1;k++){
+                    arr[k]=arr[k+1];
+                }
+                n--;
+                j--;
+            }
+        }
+    }
+    cout<<"array after removing duplicates is";
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+
+    }
+    return 0;
+    
+}
